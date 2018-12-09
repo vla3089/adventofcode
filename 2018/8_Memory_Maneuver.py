0aefb5509e9f816f89eeab703393be7222632e02
vlad <vla3089@gmail.com>
2018-12-10 00:20:47 +0300

    8_1 optimization

--- 2018/8_Memory_Maneuver.py
+++ 2018/8_Memory_Maneuver.py
@@ -3,20 +3,22 @@
 def task1(input):
     meta_sum = 0
     stack = []
-    tail = input
-    while tail or stack:
+    pos = 0
+    while pos < len(input) or stack:
         if stack:
             child_nodes, meta_entries = stack.pop()
             if child_nodes == 0:
-                for i in range(meta_entries):
-                    meta_sum += tail[i]
-                tail = tail[meta_entries:]
+                for i in range(pos, pos + meta_entries):
+                    meta_sum += input[i]
+                pos += meta_entries
             else:
                 stack.append((child_nodes - 1, meta_entries))
-                child_nodes, meta_entries, tail = tail[0], tail[1], tail[2:]
+                child_nodes, meta_entries = input[pos], input[pos + 1]
+                pos += 2
                 stack.append((child_nodes, meta_entries))
         else:
-            child_nodes, meta_entries, tail = tail[0], tail[1], tail[2:]
+            child_nodes, meta_entries = input[pos], input[pos + 1]
+            pos += 2
             stack.append((child_nodes, meta_entries))
     return meta_sum
 
